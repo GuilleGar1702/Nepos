2c58b09b9d9c5fcd722672b2f741b79eb1801c98
Guillermo <ggarcia.5065@unimar.edu.ve>
2024-05-29 01:14:13 -0400

    se mejoró la interfaz, y el prompt

--- JS/inputBar.js
+++ JS/inputBar.js
@@ -1,18 +1,18 @@
-const userPrompt = document.getElementById('userPrompt');
+const userText = document.getElementById('userPrompt');
 const maxLines=200;
 
- userPrompt.addEventListener('input', function() {
+userText.addEventListener('input', function() {
 
     if (this.scrollHeight<maxLines) {
         this.style.height=this.scrollHeight+'px';
     }
 })
 
-userPrompt.addEventListener('keydown', function(event) {
+userText.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         event.preventDefault();
-        userPrompt.value = '';
-        userPrompt.style.height = 'auto';
+        userText.value = '';
+        userText.style.height = 'auto';
     }
 });
 
