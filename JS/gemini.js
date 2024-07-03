2c0e788c8472efad8a87303bcf87d6b2bc1a1789
Guillermo <ggarcia.5065@unimar.edu.ve>
2024-07-02 22:22:46 -0400

    cambios esteticos y pdf

--- JS/gemini.js
+++ JS/gemini.js
@@ -1,4 +1,33 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+const chatHistory = [];
+let pdfText;
+
+
+  function downloadPDF() {
+
+    for (let i = 0; i < chatHistory.length; i++) {
+      if (i%2==0) {
+        chatHistory[i]="Usuario: "+ chatHistory[i];
+      } else {
+        chatHistory[i]="Nepos: "+chatHistory[i];
+      }
+      }
+      const finalText = chatHistory.join("\n\n");
+      const document = {
+        content: [
+          { text: finalText, style: 'normal' } 
+        ],
+        styles: {
+          style: { fontSize: 12, font: 'times' }
+        }
+      };
+
+      pdfMake.createPdf(document).download('nepos.pdf');
+    }
+    
+
+
+
 async function run() {
   async function chatting() {
     const userResponse = document.createElement('span')
@@ -11,6 +40,7 @@ async function run() {
     });
   
     console.log('User:', prompt);
+    chatHistory.push(prompt);
     const result = await chat.sendMessage(prompt);
     const response = await result.response;
     const text = response.text();
@@ -22,6 +52,7 @@ async function run() {
     messages.appendChild(neposResponse);
   
     console.log('AI:', text);
+    chatHistory.push(text);
     prompt = '';
     window.scrollTo({
         top: document.body.scrollHeight,
@@ -30,8 +61,16 @@ async function run() {
   }
 
 
-const API_KEY = "AIzaSyBOI0Y7S8O-AU3GMD1iwnKfQTK_5fabPkE";
+// const API_KEY = "AIzaSyBOI0Y7S8O-AU3GMD1iwnKfQTK_5fabPkE";
 // const API_KEY = "AIzaSyBxytF5KX3I5AvUV__KLML7zx9wu00hDoU";
+// const API_KEY = "AIzaSyBmEeYaIKu-7nkdWCmsI5208WOdjkU3Prw";
+
+// const API_KEY = "AIzaSyCJOhUe5BLoqPYB7BvMvOvVAvQWIKsNquM";
+
+
+
+
+
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 // const model = genAI.getGenerativeModel({ model: "gemini-pro" });
@@ -54,11 +93,13 @@ const userPrompt = document.getElementById('userPrompt');
 const messages =document.getElementById('messages');
 const sendButton = document.getElementById('sendButton');
 let prompt = '';
-const parameters1 = 'No uses negritas, cursiva ni italica para escribir. Responde en español, por favor. Tu nombre será Nepos y eres un asistente para una pagina sobre reseñas de anime, series y peliculas que sabe todo sobre el tema y puede recomendar en base a los gustos y preferencias del usuario. No debes usar nunca palabras ofencivas, lenguaje inapropiado y debes seguir los protocolos de seguridad de google/gemini para no generar una respuesta que pueda ser bloqueda por seguridad. Elimina cualquier contenido que pueda considerarse ofensivo, discriminatorio, violento, de odio, o que promueva actividades ilegales o dañinas. Si te hablan de romance o amor, di que no entiendes. Tu respuesta no excedera 50 palabras.';
+const parameters1 = 'Responde en español. Tu nombre será Nepos y eres un asistente para una pagina sobre reseñas de anime, series y peliculas que sabe todo sobre el tema y puede recomendar en base a los gustos del usuario. No debes usar nunca palabras lenguaje inapropiado y debes seguir los protocolos de seguridad de google/gemini para no generar una respuesta que pueda ser bloqueda por seguridad. Elimina cualquier contenido que pueda considerarse ofensivo, discriminatorio, violento, de odio, o que promueva actividades ilegales o dañinas.';
 
-const parameters2 = 'Si te preguntan algo fuera del tema para el que estás diseñado, respondes a la pregunta, pero luego dejas claro para lo que fuiste entrenado';
+const parameters2 = 'Si te preguntan algo fuera del tema para el que estás diseñado, respondes a la pregunta, pero luego dejas claro para lo que fuiste entrenado. Tu respuesta no excedera 40 palabras. No uses negritas, cursiva ni italica para escribir. Si te hablan de romance o amor, di que no entiendes.';
 chat.sendMessage(parameters1);
-chat.sendMessage(parameters2);
+//chat.sendMessage(parameters2);
+
+
 try {
     userPrompt.addEventListener('input', function() {
     prompt = userPrompt.value;
@@ -67,6 +108,7 @@ try {
     userPrompt.addEventListener('keydown', async function(event) {
       if (event.key === 'Enter' && prompt != '') {
         chatting();
+        
           /* const userResponse = document.createElement('span')
           userResponse.classList.add('userBubble');
           userResponse.textContent=prompt;
@@ -108,4 +150,17 @@ try {
     console.error('Error:', error);
 }
 }
+
+
+
+
+pdfExport.addEventListener('click', () => {
+  downloadPDF();
+});
+
+
+
+
+
+
 run();
--- JS/menu.js
+++ JS/menu.js
@@ -1,6 +1,9 @@
 const bar1 = document.getElementById('bar1');
 const bar2 = document.getElementById('bar2');
 const bar3 = document.getElementById('bar3');
+const sideMenu = document.getElementById('sideMenu');
+const options = document.getElementById('options');
+options.classList.add('unanimated-option');
 
 menu.addEventListener('click', () => {
     if(!bar1.classList.contains('animated-button1')){
@@ -10,6 +13,14 @@ menu.addEventListener('click', () => {
         bar1.classList.add('animated-button1');
         bar2.classList.add('animated-button2');
         bar3.classList.add('animated-button3');
+
+        
+        sideMenu.classList.remove('unanimated-menu');
+        sideMenu.classList.add('animated-menu');
+
+        // options.classList.remove('unanimated-option');
+        // options.classList.add('animated-option');
+
     }
     else{
         bar1.classList.remove('animated-button1');
@@ -18,7 +29,15 @@ menu.addEventListener('click', () => {
         bar1.classList.add('unanimated-button1');
         bar2.classList.add('unanimated-button2');
         bar3.classList.add('unanimated-button3');
+
+
+        sideMenu.classList.remove('animated-menu');
+        sideMenu.classList.add('unanimated-menu');
+
+        // options.classList.remove('animated-option');
+        // options.classList.add('unanimated-option');
     }
     
   });
+
 
