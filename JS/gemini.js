70df5874a77d7f0bbb645cf2a8504cfe3eabd5e8
Guillermo <ggarcia.5065@unimar.edu.ve>
2024-07-14 15:28:33 -0400

    se cambió la estetica de la interfaz, se mejoró la busqueda de mensajes, el menu ahora se oculta al presionar fuera del menu

--- JS/gemini.js
+++ JS/gemini.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-const chatHistory = [];
+let chatHistory = [];
 let pdfText;
-
+let searchIndex=0;
 
   function downloadPDF() {
 
@@ -21,53 +21,75 @@ let pdfText;
           style: { fontSize: 12, font: 'times' }
         }
       };
-
-      pdfMake.createPdf(document).download('nepos.pdf');
+      if (chatHistory.length>0) {
+        pdfMake.createPdf(document).download('nepos.pdf');
+      } else {
+        console.log("No messages to export");
+      }
+      
     }
     
+    function findMessage() {
+      try {
+        let found=[];
+        let searchText=searchInput.value;
+        chatHistory.forEach(message => {
+          if (message.includes(searchText)){
+            found.push(chatHistory.findIndex(messageF => messageF.includes(message)));
+          }
+        });
+        let index=found.length-1-searchIndex;
+        if (index<found.length) {
+          let matches= document.getElementsByClassName('message');
+          let Y1 = window.scrollY;
+          if (matches[found[0]]) {
+            matches[found[0]].scrollIntoView();
+          }
+          
+          let Y2 = window.scrollY;
+          if (Y1>Y2){
+            window.scrollBy(0, -200);
+          }
+          if (Y1<Y2){
+            window.scrollBy(0, 200);
+          }
+          if (Y1=Y2){
+            window.scrollBy(0, 0);
+          }
+          if (matches[found[index]].classList.contains('userBubble')) {
+            matches[found[index]].classList.add('focusU');
+            setTimeout(() => {
+              matches[found[index]].classList.remove('focusU');
+            }, 2100);
+          } else {
+            matches[found[index]].classList.add('focusN');
+            setTimeout(() => {
+              matches[found[index]].classList.remove('focusN');
+            }, 2100);
+          }
+          console.log(found);
+        } else {
+          console.log("out of match array");
+          searchIndex--;
+        }
+          
+        
+      } catch (error) {
+        console.log("not found", "\n", error);
+      }
+
+      
+      
+    }
 
 
-
-async function run() {
-  async function chatting() {
-    const userResponse = document.createElement('span')
-    userResponse.classList.add('userBubble');
-    userResponse.textContent=prompt;
-    messages.appendChild(userResponse);
-    window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: 'smooth'
-    });
-  
-    console.log('User:', prompt);
-    chatHistory.push(prompt);
-    const result = await chat.sendMessage(prompt);
-    const response = await result.response;
-    const text = response.text();
-  
-    
-    const neposResponse = document.createElement('span')
-    neposResponse.classList.add('neposBubble');
-    neposResponse.textContent=text;
-    messages.appendChild(neposResponse);
-  
-    console.log('AI:', text);
-    chatHistory.push(text);
-    prompt = '';
-    window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: 'smooth'
-    });
-  }
 
 
 // const API_KEY = "AIzaSyBOI0Y7S8O-AU3GMD1iwnKfQTK_5fabPkE";
 // const API_KEY = "AIzaSyBxytF5KX3I5AvUV__KLML7zx9wu00hDoU";
 // const API_KEY = "AIzaSyBmEeYaIKu-7nkdWCmsI5208WOdjkU3Prw";
 
-// const API_KEY = "AIzaSyCJOhUe5BLoqPYB7BvMvOvVAvQWIKsNquM";
-
-
+const API_KEY = "AIzaSyCJOhUe5BLoqPYB7BvMvOvVAvQWIKsNquM";
 
 
 
@@ -93,50 +115,56 @@ const userPrompt = document.getElementById('userPrompt');
 const messages =document.getElementById('messages');
 const sendButton = document.getElementById('sendButton');
 let prompt = '';
-const parameters1 = 'Responde en español. Tu nombre será Nepos y eres un asistente para una pagina sobre reseñas de anime, series y peliculas que sabe todo sobre el tema y puede recomendar en base a los gustos del usuario. No debes usar nunca palabras lenguaje inapropiado y debes seguir los protocolos de seguridad de google/gemini para no generar una respuesta que pueda ser bloqueda por seguridad. Elimina cualquier contenido que pueda considerarse ofensivo, discriminatorio, violento, de odio, o que promueva actividades ilegales o dañinas.';
+const parameters1 = 'Responde en español. Tu nombre será Nepos y eres un asistente para una pagina sobre reseñas de anime, series y peliculas que sabe todo sobre el tema y puede recomendar en base a los gustos del usuario. No debes usar nunca palabras lenguaje inapropiado y debes seguir los protocolos de seguridad de google/gemini para no generar una respuesta que pueda ser bloqueda por seguridad. Elimina cualquier contenido que pueda considerarse ofensivo, discriminatorio, violento, de odio, o que promueva actividades ilegales o dañinas. Tu respuesta no excedera 40 palabras.';
 
 const parameters2 = 'Si te preguntan algo fuera del tema para el que estás diseñado, respondes a la pregunta, pero luego dejas claro para lo que fuiste entrenado. Tu respuesta no excedera 40 palabras. No uses negritas, cursiva ni italica para escribir. Si te hablan de romance o amor, di que no entiendes.';
 chat.sendMessage(parameters1);
-//chat.sendMessage(parameters2);
+// chat.sendMessage(parameters2);
+
+
+async function run() {
+  async function chatting() {
+    const userResponse = document.createElement('span')
+    userResponse.classList.add('userBubble');
+    userResponse.classList.add('message');
+    userResponse.textContent=prompt;
+    messages.appendChild(userResponse);
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+  
+    console.log('User:', prompt);
+    chatHistory.push(prompt);
+    const result = await chat.sendMessage(prompt);
+    const response = await result.response;
+    const text = response.text();
+  
+    
+    const neposResponse = document.createElement('span')
+    neposResponse.classList.add('neposBubble');
+    neposResponse.classList.add('message');
+    neposResponse.textContent=text;
+    messages.appendChild(neposResponse);
+  
+    console.log('AI:', text);
+    chatHistory.push(text);
+    prompt = '';
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+  }
 
 
 try {
     userPrompt.addEventListener('input', function() {
-    prompt = userPrompt.value;
+      prompt = userPrompt.value;
     });
 
     userPrompt.addEventListener('keydown', async function(event) {
       if (event.key === 'Enter' && prompt != '') {
         chatting();
-        
-          /* const userResponse = document.createElement('span')
-          userResponse.classList.add('userBubble');
-          userResponse.textContent=prompt;
-          messages.appendChild(userResponse);
-
-          console.log('User:', prompt);
-          const result = await chat.sendMessage(prompt, parameters);
-          const response = await result.response;
-          const text = response.text();
-
-          console.log('AI:', text);
-          prompt = '';
-          window.scrollTo({
-              top: document.body.scrollHeight,
-              behavior: 'smooth'
-          });
-
-          const neposResponse = document.createElement('span')
-          neposResponse.classList.add('neposBubble');
-          neposResponse.textContent=text;
-          messages.appendChild(neposResponse);
-
-          console.log('AI:', text);
-          prompt = '';
-          window.scrollTo({
-              top: document.body.scrollHeight,
-              behavior: 'smooth'
-          }); */
       }
     });
 
@@ -151,16 +179,38 @@ try {
 }
 }
 
-
-
-
+const pdfExport = document.getElementById('pdfExport');
 pdfExport.addEventListener('click', () => {
   downloadPDF();
 });
 
+/* const find = document.getElementById('find');
+find.addEventListener('click', () => {
+  findMessage();
+}); */
+
+const newChat = document.getElementById('newChat');
+newChat.addEventListener('click', () => {
+  location.reload();
+});
 
 
-
-
+const searchInput = document.getElementById('searchInput');
+searchInput.addEventListener('input', function() {
+  searchIndex=0;
+  findMessage();
+});
+const btnN = document.getElementById('btnN');
+btnN.addEventListener('click', function() {
+  searchIndex++;
+  findMessage();
+});
+const btnP = document.getElementById('btnP');
+btnP.addEventListener('click', function() {
+  if (searchIndex>0) {
+    searchIndex--;
+    findMessage();
+  }
+});
 
 run();
--- JS/menu.js
+++ JS/menu.js
@@ -3,9 +3,18 @@ const bar2 = document.getElementById('bar2');
 const bar3 = document.getElementById('bar3');
 const sideMenu = document.getElementById('sideMenu');
 const options = document.getElementById('options');
-options.classList.add('unanimated-option');
+const searchInput = document.getElementById('searchInput');
+const find = document.getElementById('find');
+const userInput = document.getElementById('userInput');
+const messages = document.getElementById('messages');
+const chatBody = document.getElementById('chatBody');
+const Logo = document.getElementById('logo');
+const menu = document.getElementById('menu');
+const search = document.getElementById('search');
+const btnNP = document.getElementsByClassName('btnNP');
 
-menu.addEventListener('click', () => {
+
+function menuInteraction() {
     if(!bar1.classList.contains('animated-button1')){
         bar1.classList.remove('unanimated-button1');
         bar2.classList.remove('unanimated-button2');
@@ -17,9 +26,6 @@ menu.addEventListener('click', () => {
         
         sideMenu.classList.remove('unanimated-menu');
         sideMenu.classList.add('animated-menu');
-
-        // options.classList.remove('unanimated-option');
-        // options.classList.add('animated-option');
 
     }
     else{
@@ -34,10 +40,98 @@ menu.addEventListener('click', () => {
         sideMenu.classList.remove('animated-menu');
         sideMenu.classList.add('unanimated-menu');
 
-        // options.classList.remove('animated-option');
-        // options.classList.add('unanimated-option');
-    }
-    
-  });
+        
+        
+        if (searchInput.classList.contains('inSearch')) {
+            searchInput.classList.remove('inSearch');
+            
+            searchInput.classList.add('unSearch');
+            
+            
+            setTimeout(() => {
+                searchInput.classList.remove('niceBorder');
+                // searchInput.classList.add('invisible');
+                search.classList.add('invisible');
+                
 
+
+              }, 301);
+            
+        }
+        if (Logo.classList.contains('inSearchLogo')) {
+            Logo.classList.remove('inSearchLogo');
+            Logo.classList.add('unSearchLogo');
+            
+        }
+        
+    }
+}
+
+function findInteraction() {
+    if(sideMenu.classList.contains('animated-menu')){
+        sideMenu.classList.remove('animated-menu');
+        sideMenu.classList.add('unanimated-menu');
+
+        searchInput.classList.remove('unSearch');
+        // searchInput.classList.remove('invisible');
+        search.classList.remove('invisible');
+
+        searchInput.classList.add('inSearch');
+        searchInput.classList.add('niceBorder');
+
+        Logo.classList.remove('unSearchLogo');
+        Logo.classList.add('inSearchLogo');
+    }
+}
+
+
+function gestureHider() {
+    if(bar1.classList.contains('animated-button1')){
+        bar1.classList.remove('animated-button1');
+        bar2.classList.remove('animated-button2');
+        bar3.classList.remove('animated-button3');
+        bar1.classList.add('unanimated-button1');
+        bar2.classList.add('unanimated-button2');
+        bar3.classList.add('unanimated-button3');
+
+
+        sideMenu.classList.remove('animated-menu');
+        sideMenu.classList.add('unanimated-menu');
+
+        searchInput.classList.remove('inSearch');
+        if (!searchInput.classList.contains('unSearch')) {
+            searchInput.classList.add('unSearch');
+            
+            setTimeout(() => {
+                searchInput.classList.remove('niceBorder');
+                // searchInput.classList.add('invisible');
+                search.classList.add('invisible');
+              }, 301);
+            
+        }
+        if (Logo.classList.contains('inSearchLogo')) {
+            Logo.classList.remove('inSearchLogo');
+            Logo.classList.add('unSearchLogo');
+            
+        }
+    }
+}
+
+
+
+
+menu.addEventListener('click', () => {
+    menuInteraction();
+});
+
+find.addEventListener('click', () => {
+    findInteraction();    
+});
+
+userInput.addEventListener('click', () =>{
+    gestureHider();
+});
+chatBody.addEventListener('click', () =>{
+    gestureHider();
+});
 
